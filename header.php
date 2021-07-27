1b55a22e19b116232de659fec6ad7cf284f02f01
Josh <joshthomson15@gmail.com>
2021-07-27 13:58:00 +0100

    Add booking system / styling

--- header.php
+++ header.php
@@ -8,6 +8,9 @@
 
     <?php wp_head(); ?>
 
+    <script src="https://pxportal.xeyex.co.uk/ob/x3edabac23446401cfg"></script>
+    <script src="https://pxportal.xeyex.co.uk/ob/x3edabac23446401"></script>
+
 </head>
 
 <body <?php body_class(); ?>>
@@ -25,6 +28,19 @@
 
 		<header class="header">
 
+            <div class="pre__header">
+                <div class="max__width">
+                    <ul class="contact">
+                        <li><i class="fa fa-phone"></i><a href="tel:01463782323">01463 78 23 23</a></li>
+                        <li><i class="fa fa-envelope"></i><a href="mailto:reception@fraseroptical.co.uk">Email Us</a></li>
+                    </ul>
+
+                    <div class="book_button">
+                        <a href="/book-appointment/" class="book">Book Appointment</a>
+                    </div>
+                </div>
+            </div>
+
             <div class="header__main">
 
                 <div class="max__width">
@@ -32,17 +48,12 @@
                     <div class="header_logo_menu">
                         <div class="logo">
                             <a href="<?php echo esc_url(home_url()); ?>" title="<?php bloginfo('name'); ?>">
-                                <img src="<?php echo esc_url(get_stylesheet_directory_uri()); ?>/img/logo.jpg" alt="<?php bloginfo('name'); ?>">
+                                <img src="https://fraser-optical.twsbeta3.co.uk/wp-content/uploads/2021/07/fraser-logo-e1627306300113.jpeg" alt="<?php bloginfo('name'); ?>">
                             </a>
                         </div><!-- logo -->
                     </div>
 
                     <div class="header__cta">
-
-                        <ul class="contact">
-                            <li><i class="fa fa-phone"></i><a href="tel:01463782323">01463 78 23 23</a></li>
-                            <li><i class="fa fa-envelope"></i><a href="mailto:reception@fraseroptical.co.uk">Email Us</a></li>
-                        </ul>
 
                         <div class="header__menu">
                             <?php wp_nav_menu(array('menu' => 'Main Menu', 'container' => false)); ?>
